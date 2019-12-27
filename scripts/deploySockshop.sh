b090d2b2c22d08c4da48b5d221d69a5c44939e1c
collin.lesko <lesko1cj@alma.edu>
2019-12-27 08:42:28 -0500

    updated directories

--- scripts/deploySockshop.sh
+++ scripts/deploySockshop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ../../repositories/sockshop-infrastructure
+cd ../../sockshop-infrastructure
 kubectl create -f manifests/    
 kubectl apply -f manifests/carts-db.yaml
 kubectl apply -f manifests/catalogue-db.yaml
